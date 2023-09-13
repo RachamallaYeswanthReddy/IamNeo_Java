d0e8f63b7b148322423141855cb9dec9d1e7fca8
Yeswanth Reddy R <yeswanthrachamalla@gmail.com>
2023-09-13 16:48:32 +0530

    Day-2

---
+++ src/Variable.java
@@ -0,0 +1,24 @@
+public class Variable {
+    //instance variable
+    String instvar = "I am instance var";
+    int instint;
+    //Static Var
+    static int STATICVAR = 100;
+    static String STATICSTRING;
+    //local variable
+    public void localmethod(){
+        int localvar= 20;
+        System.out.println("Local Variable: " + localvar);
+    }
+    public static void main(String[] args) {
+        Variable vr = new Variable();
+
+        System.out.println(vr.instvar);
+        System.out.println(vr.instint);
+
+        System.out.println(STATICSTRING);
+        System.out.println(STATICVAR);
+
+        vr.localmethod();
+    }
+}
