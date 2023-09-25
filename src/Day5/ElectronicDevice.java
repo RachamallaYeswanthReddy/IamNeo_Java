14b125c585e50bfbd90b2591a60ebbadfdf1752f
Yeswanth Reddy R <yeswanthrachamalla@gmail.com>
2023-09-25 15:05:44 +0530

    Day-5

--- src/Day5/ElectronicDevice.java
+++ src/Day5/ElectronicDevice.java
@@ -1,7 +1,36 @@
 package Day5;
 
-public class ElectronicDevice {
-    public static void main(String[] args) {
-        
+interface ElectronicDevice {
+    void turnoff();
+    void turnon();
+}
+interface Display{
+    void DisplayDevice();
+}
+class Telvision implements ElectronicDevice,Display{
+    @Override
+    public void turnoff(){
+        System.out.println("The TV is Turnoff");
+    }
+    @Override
+    public void turnon(){
+        System.out.println("The TV is TurnOn");
+    }
+    public void DisplayDevice(){
+        System.out.println("Display");
+    }
+}
+//concrete class
+class Smartphone implements ElectronicDevice,Display{
+    @Override
+    public void turnoff(){
+        System.out.println("Booting off");
+    }
+    @Override
+    public void turnon(){
+        System.out.println("Booting On");
+    }
+    public void DisplayDevice(){
+        System.out.println("Display");
     }
 }
